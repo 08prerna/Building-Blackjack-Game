cbed703832b9dc422627a84dd7dd5e568eb51baa
Prerana Dhayfule <132763290+08prerna@users.noreply.github.com>
2023-11-20 12:39:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
-let firstcard=10
-let secondcard=4
-let sum=firstcard+secondcard
-let hasBlackjack=false
-let islive=true
-let message=""
-let mssger= document.getElementById("mess-error")
+let cards=[]
+let sum=0
+let hasBlackjack = false
+let islive = false
+let message = ""
+let mesgErr = document.getElementById("mess-error")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
 
 function srtGame() {
-    if(sum<=20){
-        message="Do you want to draw a new card?"
-    }
-    else if(sum===21){
-        message="You've got Blackjack!"
-        hasBlackjack=true
-    }
-    else{
-        message="You're out of the game!"
-        islive=false
+    islive = true
+    let firstcard = getRandomCard()
+    let secondcard = getRandomCard()
+    cards = [firstcard, secondcard]
+    sum = firstcard + secondcard
+    renderGame()
+}
+
+function renderGame() {
+    cardEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardEl.textContent += cards[i] + " "
     }
 
-    mssger.textContent=message
+    sumEl.textContent = "Sum: " + sum
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackjack = true
+    } else {
+        message = "You're out of the game!"
+        islive = false
+    }
+
+    mesgErr.textContent = message
+}
+
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+
+}
+
+function getRandomCard() {
+    let number = Math.floor(Math.random() * 13) + 1
+    if (number > 10) {
+        return 10
+    } else if (number === 1) {
+        return 11
+    } else {
+        return number
+    }
 }
